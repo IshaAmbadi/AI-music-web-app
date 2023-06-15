564f25ca0fa41fcffa0412cb43ce599e415efdd8
IshaAmbadi <87016718+IshaAmbadi@users.noreply.github.com>
2023-06-15 19:32:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,50 @@
 track1 = "";
 track2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     track1 = loadSound("funkmusicTrack1.mp3");
     track2 = loadSound("swingmusicTrack2.mp3")
 }
 
 function setup() {
-    canvas = createCanvas(1279, 720);
+    canvas = createCanvas(600, 500);
     canvas.center();
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotposes)
 }
 
 function draw() {
-    image(video, 0, 0, 1279, 720)
+    image(video, 0, 0, 600, 500)
 
+}
+
+function play() {
+    track1.play();
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function gotposes(results) {
+    if(results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + "leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + "rightWristY = " + rightWristY);
+    }
 }
